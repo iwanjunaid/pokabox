a9544ee52fadc2365bd82eec2988cf5627f7c5f8
Kurniawan Junaidy <iwan.junaid@gmail.com>
2020-11-04 09:02:57 +0700

    Add example for handling event StatusChanged.

--- main.go
+++ main.go
@@ -51,6 +51,8 @@ func main() {
 			fmt.Printf("%v\n", event)
 		case events.Fetched:
 			fmt.Printf("%v\n", event)
+		case events.StatusChanged:
+			fmt.Printf("%v\n", event)
 		case events.PickerPaused:
 			fmt.Printf("%v\n", event)
 		}
