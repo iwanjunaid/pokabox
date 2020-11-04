f0bdb74647adf563378e10441d70f91fc9920f12
Kurniawan Junaidy <iwan.junaid@gmail.com>
2020-11-04 14:05:35 +0700

    Implement interface Event in event PickerPaused.

--- event/pickerpaused.go
+++ event/pickerpaused.go
@@ -2,18 +2,24 @@ package event
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/google/uuid"
 )
 
 type PickerPaused struct {
-	GroupID uuid.UUID
+	PickerGroupID uuid.UUID
+	Timestamp     time.Time
 }
 
 func (p PickerPaused) String() string {
-	return fmt.Sprintf("[%s:%s] Picker paused", PREFIX, p.GroupID)
+	return fmt.Sprintf("[%s:%s] Picker paused", PREFIX, p.PickerGroupID)
 }
 
-func (p PickerPaused) GetGroupID() uuid.UUID {
-	return p.GroupID
+func (p PickerPaused) GetPickerGroupID() uuid.UUID {
+	return p.PickerGroupID
+}
+
+func (p PickerPaused) GetTimestamp() time.Time {
+	return p.Timestamp
 }
